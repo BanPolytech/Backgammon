e10299c470c8ae9e0211bfdcf58b899cca6da962
Drake <drake@MacBook-Pro-de-Drake.local>
2017-02-03 16:28:06 +0100

    gameplay.h maj

---
+++ gameplay.h
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "affichage.h"
+
+#define TMAX 25
+#define IA 0
+#define U1 1
+#define U2 2
+
+typedef struct coup {
+	int joueur;
+	int depart;
+	int deplacement;
+}coup, *tpc;
+
+
+int lancer_de(void);
+void tour_joueur(int T[], int J);
+coup definir_coup(int joueur, int depart, int deplacement);
+
+
+void tour_joueur(int T[], int J) {
+
+	int pos, de1, de2;
+
+    srand(time(NULL)); // initialisation de rand
+	de1 = lancer_de();
+	de2 = lancer_de();
+
+	tpc coups = coups_possibles(T, J);
+	display_pions_possibles(T);
+
+	pos = choix_coup(T, J);
+	display_coups_possibles(T, pos);
+
+	tpc choix_deplacement(T, de1, de2, pos);
+
+
+
+
+}
+
+
+int lancer_de(void) {
+
+    int nombre_aleatoire = 0;
+    
+	return nombre_aleatoire = rand()%6+1;
+}
+
+int choix_coup(int T[], int joueur)
+{
+	int pos;
+
+	do {
+		printf("Quel pion voulez-vous jouer?\nVeuillez indiquer le numéro de colonne :");
+		scanf("%d",&pos);
+		printf("\n");
+	}while (T[pos] < 0);
+	printf("\n\n");
+	return pos;
+}
+
+tpc choix_deplacement(int T[], int de1, int de2, tpc c, int pos) {
+
+	int i = 0;
+	coup poss[];
+
+	printf("Veuillez indiquer le coup que vous voulez jouer parmi ceux possibles\n");
+
+	while(c[i]) {
+		if ((c[i].depart == pos && c[i].deplacement == de1) || (c[i].depart == pos && c[i].deplacement == de2) || (c[i].depart == pos && c[i].deplacement == de1 + de2)) {
+
+		}
+		i++;
+	}
+
+}
+
+
+tpc coups_possibles(int T[], int joueur) {
+
+	coup poss[];
+	int k = 0;
+	int combinaisons[21][2] = {{1,1},{1,2},{1,3},{1,4},{1,5},{1,6},{2,2},{2,3},{2,4},{2,5},{2,6},{3,3},{3,4},{3,5},{3,6},{4,4},{4,5},{4,6},{5,5},{5,6},{6,6}};
+
+	for (int i = 0; i < TMAX; ++i) {
+		if((joueur == IA || joueur == U2) && T[i] > 0) {
+			for (int j = 0; j < 20; ++j) {
+				if(T[i + combinaisons[j][0]] >= 0) {
+					poss[k] = definir_coup(joueur, i, combinaisons[j][0]);
+					k++;
+				} else if(T[i + combinaisons[j][1]] >= 0) {
+					poss[k] = definir_coup(joueur, i, combinaisons[j][1]);
+					k++;
+				}
+			}
+		} else if (joueur == U1 && T[i] < 0) {
+			for (int j = 0; j < 20; ++j) {
+				if(T[i + combinaisons[j][0]] <= 0) {
+					poss[k] = definir_coup(joueur, i, combinaisons[j][0]);
+					k++;
+				} else if(T[i + combinaisons[j][1]] <= 0) {
+					poss[k] = definir_coup(joueur, i, combinaisons[j][1]);
+					k++;
+		}
+	}
+
+	return poss;
+}
+
+coup definir_coup(int joueur, int depart, int deplacement) {
+	coup c;
+	c = (*coup)malloc(sizeof(coup));
+	c.joueur = joueur;
+	c.depart = depart;
+	c.deplacement = deplacement;
+
+	return c;
+}
--- main.c
+++ main.c
@@ -1,46 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "affichage.h"
-/*
-void display(int T[24][2])
-{
-	int i,j,k;
-	
-	k=0;
-	printf(" __________________________\n");
-	for (j=1;j=7;j++)
-	{
-		if (T[k][0] == 1)
-			printf("0 ");
-		else	
-			printf("  ");
-		k++;		
-	}
-	for (i=2;i<14;i++)
-	{
-		printf("|");
-		for (j=1;j<7;j++)
-		{	
-			if (T[k][0] == 1)
-				printf("0 ");
-			else	
-				printf("  ");
-			k++;
-		}
-		printf("|");
-		for (j=8;j<14;j++)
-		{	
-			if (T[k][0] == 1)
-				printf("0 ");
-			else	
-				printf("  ");
-			k++;
-		}
-		printf(" | %d\n",k);
-	}
-	printf(" TTTTTTTTTTTTTTTTTTTTTTTTTT\n");
-}
-*/
+#include "gameplay.h"
+
+#define IA 0
+#define U1 1
+#define U2 2
+
 void initialisation() {
 
 	//il faut mettre les pions de bases
@@ -50,26 +16,50 @@ void initialisation() {
 
 }
 
+typedef struct coup {
+	int joueur;
+	int depart;
+	int deplacement;
+}coup;
+
+
 
 int main(void)
 {
-	int T[26][2]={{0},{0}};
+	int T[25]={0};
+	int Cimetiere_IA;
+	int Cimetiere_U;
+	int Sortie_IA;
+	int Sortie_U;
+	int win = 0;
+	int c;
 
-	initialisation();
-	afficher(T);
-	display(T);
-	return 0;
-}
-/*
-int main(void)
-{
-	int i;
-	int tableau[10][2];
-	for (i = 0; i < 10; i++)
-	{
-	    tableau[i][1] = i;
+	printf("Jouez vous contre un autre joueur(0) ou contre l'IA(1) ?\n");
+	scanf("%d",&c);
+
+	if(c == 0) {
+		while(!win){
+
+			initialisation(T);
+			display(T);
+			tour_joueur(T, U1);
+			display(T);
+			tour_joueur(T, U2);
+
+			
+		}
+	} else if(c == 1) {
+		while(!win){
+
+			initialisation(T);
+			display(T);
+			tour_joueur(T, U1);
+			display(T);
+			tour_joueur(T, IA);
+
+			
+		}
 	}
-	printf("%d",tableau[9][1]);
 	return 0;
 }
-*/
+
