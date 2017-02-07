0ee8f4aadc4d4df995cff6cc7290693482403dea
BanPolytech <esteban.gobert@me.com>
2017-02-07 16:43:12 +0100

    Update couleurs.h

--- couleurs.h
+++ couleurs.h
@@ -1,25 +1,17 @@
-<<<<<<< HEAD
+
 //#include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-
 #ifndef COULEURS
 #define COULEURS
 
-=======
-#ifndef COULEURS
-#define COULEURS
-
-#include <stdio.h>
-
->>>>>>> master
 #define clrscr() printf("\033[H\033[2J")
 #define couleur(param) printf("\033[%sm",param)
 
 
 #endif
-<<<<<<< HEAD
+
 /*
 void color(int t,int f)
 {
@@ -27,6 +19,4 @@ void color(int t,int f)
             SetConsoleTextAttribute(H,f*16+t);
 }
 */
-=======
 
->>>>>>> master
